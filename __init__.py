51b7ebb21d03c651d396d18d18c1f245ff1b72d5
yanxx422 <yanxx401@umn.edu>
2021-04-17 22:04:50 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,32 +7,32 @@ class Engine(Cmd):
     def __init__(self):
         Cmd.__init__(self)
 
-    def action_create(self:str):
+    def do_create(self:str):
         try:
             create(arg):
         except Exception ('Creating Failed.')
 
-    def action_drop(self,arg:str):
+    def do_drop(self,arg:str):
         try:
             drop(arg):
         except Exception ('Dropping Failed.')
 
-    def action_insert(self,arg:str):
+    def do_insert(self,arg:str):
         try:
             insert(arg):
         except Exception ('Inserting Failed.')
 
-    def action_delete(self,arg:str):
+    def do_delete(self,arg:str):
         try:
             delete(arg):
         except Exception ('Deleting Failed.')
 
-    def action_show(self,arg:str):
+    def do_show(self,arg:str):
         try:
             show(arg):
         except Exception ('Showing Failed.')
 
-    def action_exit(self,arg:str):
+    def do_exit(self,arg:str):
         print("See you.")
         return True
 
